ca48311a8d23ac4ae2f17a76a1b7630d02e9dbe9
Unknown <joshhaz2012@gmail.com>
2018-01-24 13:59:42 +0000

    Putting error messages at top of appointments.js

--- server/schemas/appointments.js
+++ server/schemas/appointments.js
@@ -1,32 +1,38 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+const appointment_idRequiredErrorMsg = 'Appointment ID is required.';
+const patient_idRequiredErrorMsg = 'Patient ID is required.';
+const staff_idRequiredErrorMsg = 'Staff ID is required.';
+const start_timeRequiredErrorMsg = 'Appointment start time is required';
+const end_timeRequiredErrorMsg = 'Appointment end time is required';
+
 var appointmentsSchema = new Schema({
     appointment_id: {
         type: Number,
         min: 1,
         max: 1000,
-        required: [true, 'Appointment ID is required.']
+        required: [true, appointment_idRequiredErrorMsg]
     },
     patient_id: {
         type: Number,
         min: 1,
         max: 1000,
-        required: [true, 'Patient ID is required.']
+        required: [true, patient_idRequiredErrorMsg]
     },
     staff_id: {
         type: Number,
         min: 1,
         max: 1000,
-        required: [true, 'Staff ID is required.']
+        required: [true, staff_idRequiredErrorMsg]
     },
     start_time: {
         type: Date,
-        required: [true, 'Appointment start time is required']
+        required: [true, start_timeRequiredErrorMsg]
     },
     end_time: {
         type: Date,
-        required: [true, 'Appointment end time is required']
+        required: [true, end_timeRequiredErrorMsg]
     }
 });
 
