a87fbe1796df0e0c28fe9b42d8b4a5af36d04bc3
Unknown <joshhaz2012@gmail.com>
2018-04-04 14:06:03 +0100

    Attempt to fix observables import error heroku

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 
-import { Observable } from 'rxjs/observable';
+import { Observable } from 'rxjs/Rx';
 import { interval } from 'rxjs/observable/interval';
 import { tap } from 'rxjs/operators/tap';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
